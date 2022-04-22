7865fc87fd5d1b4422d5725ef8c04123cd29bf2d
sakon-ziwave <ryo.development0320@gmail.com>
2022-04-23 00:20:38 +0900

    first-commit

--- template.php
+++ template.php
@@ -2,21 +2,18 @@
 
 <?php
 
-$a = trim(fgets(STDIN));
+$n = trim(fgets(STDIN));
 
-list($b, $c) = explode(' ', fgets(STDIN));
+list($a, $b) = explode(' ', fgets(STDIN));
 
-list($i, $j, $W, $H) = [0, 0, 2, 2];
-while ($i < $H) {
+while ($i < $n) {
     $array[$i] = explode(' ', fgets(STDIN));
     $i++;
 }
+
 $i = 0;
-while ($i < $H) {
-    echo implode(' ', $array[$i]);
-    $i++;
+while ($i < $n) {
+
 }
 
 echo PHP_EOL;
-
-?>

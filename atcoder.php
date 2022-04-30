df472872005e757be273fe10bce0690c564a6628
sakon-ziwave <ryo.development0320@gmail.com>
2022-04-30 11:47:52 +0900

    ６問目

---
+++ atcoder.php
@@ -0,0 +1,10 @@
+<?php
+$num = 0;
+for($i=1; $i< 10000; $i++){
+    for($j=1; $j< 10000; $j++){
+        $menseki[$i][$j] = $i * $j * 0.5;
+        if($menseki[$i][$j] < 5000) $num++;
+    }
+}
+
+echo $num;

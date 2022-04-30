64e744c9788c55369b51cdff14eb2b606352b23b
sakon-ziwave <ryo.development0320@gmail.com>
2022-04-30 11:57:09 +0900

    ３問目

--- atcoder.php
+++ atcoder.php
@@ -1,11 +1,8 @@
 <?php
 
-$sum = 0;
 $i = 1;
-
-while($sum < 9) {
-    $sum += 1/$i;
-    $i++;
+while($i=1; $i<=50000; $i++) {
+    if ($i % 3 == 0 || strpos(strval($i), '3') !== false) $sum += $i;
 }
 
-echo $i;
+echo $sum;

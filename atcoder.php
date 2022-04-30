aff255f5a27e37fff5528c7c8e4b49e21a213101
sakon-ziwave <ryo.development0320@gmail.com>
2022-04-30 11:53:47 +0900

    ５問目

--- atcoder.php
+++ atcoder.php
@@ -1,11 +1,24 @@
 <?php
-$num = 0;
-for($i=1; $i< 10000; $i++){
-    for($j=1; $j< 10000; $j++){
-        $menseki[$i][$j] = $i * $j * 0.5;
-        if($menseki[$i][$j] < 5000) $num++;
+
+for ($i=1; $i<=1000000; $i++) {
+    $seki[$i] = $i;
+    $kaisuu[$i] = 0;
+    for ($j=1; $j<=5; $j++) {
+        $seki[$i] = get_seki($seki[$i]);
+        if (strlen($seki[$i]) == 1) {
+            $kaisuu[$i] = $j;
+            $j = 6;
+        }
     }
 }
 
-echo $num;
-// 93643
+$answer = 0;
+for ($k=1; $k<=1000000; $k++) {
+    if ($kaisuu[$k] == 5) $answer++;
+}
+echo $answer;
+
+function get_seki($i) {
+    $array = str_split($i);
+    return array_product($array);
+}

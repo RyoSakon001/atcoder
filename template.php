26782c6f96aaf772b821bf6687fca1fe6ec1cd34
sakon-ziwave <ryo.development0320@gmail.com>
2022-05-09 01:13:44 +0900

    計測機能追加

--- template.php
+++ template.php
@@ -10,6 +10,13 @@ for ($i=0; $i<$n; $i++) {
     $array[$i] = explode(' ', trim(fgets(STDIN)));
 }
 
+$time_start = microtime(true);
 
 $answer = '';
 echo $answer.PHP_EOL;
+
+
+
+
+$time = microtime(true) - $time_start;
+echo "\n{$time}";

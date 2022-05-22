44fb3180684a5bf29acc5c52925029ec33486570
sakon-ziwave <ryo.development0320@gmail.com>
2022-05-22 14:23:54 +0900

    abc252

--- abc252_c.php
+++ abc252_c.php
@@ -1,22 +1,35 @@
-abc<?php
+<?php
 
 $n = trim(fgets(STDIN));
 
-list($a, $b) = array_map("intval", explode(" ", trim(fgets(STDIN))));
-
-$array = array_map("intval", explode(" ", trim(fgets(STDIN))));
-
 for ($i=0; $i<$n; $i++) {
-    $array[$i] = explode(' ', trim(fgets(STDIN)));
+    $array[$i] = str_split(trim(fgets(STDIN)));
 }
 
-$time_start = microtime(true);
+$duplicate_count = array_fill(0, 10, 1);
+$total_count = array_fill(0, 10, 0);
 
-$answer = '';
-echo $answer.PHP_EOL;
+// 0~9までの数字の、ダブり回数を計算する
+for ($k=0; $k<=9; $k++) { // 0~9の数値
+    for ($j=0; $j<=9; $j++) { // 左から0~9番目
+        $duplicate_count_tmp = 0;
+        for ($i=0; $i<$n; $i++) {
+            // 左から$j=0番目において、数字の$k=0が出てくる個数を数え、$duplicate_count[$j]に記録。
+            if ($array[$i][$j] == $k) {
+                $duplicate_count_tmp++;
+                $total_count[$k]++;
+            } 
+        }
+        if ($duplicate_count[$k] < $duplicate_count_tmp) {
+            $duplicate_count[$k] = $duplicate_count_tmp;
+            $last_column[$k] = $j;
+        }
+        if ($total_count[$k] == $n) {
+            if (!isset($last_column[$k])) $last_column[$k] = $j;
+            $time[$k] = ($duplicate_count[$k] - 1) * 10 + $last_column[$k];
+            continue 2;
+        }
+    }
+}
 
-
-
-
-$time = microtime(true) - $time_start;
-echo "\n{$time}";
+echo min($time).PHP_EOL;

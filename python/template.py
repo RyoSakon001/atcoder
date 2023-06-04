4c619f0a0d65068912c458dcf467bb23a15150f1
sakon-ziwave <ryo.development0320@gmail.com>
2023-06-04 09:47:00 +0900

    python

--- python/template.py
+++ python/template.py
@@ -1,5 +1,16 @@
-a = int(input())
-b, c = map(int, input().split())
+# 標準入力
+n = int(input())
+a, b = map(int, input().split())
 s = input()
+s, t = map(str, input().split())
+arr = list(map(int, input().split()))
+arr = list(map(str, input().split()))
 
-print(a + b + c, s)
+# 標準入力（複数行）
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+    arr.append(input().split())
+    arr.append(list(map(int, input().split())))
+    arr.append(list(map(str, input().split())))
+

3268f011d882ff9f80de242770a7efdddb363f8f
sakon-ziwave <ryo.development0320@gmail.com>
2023-06-11 00:06:18 +0900

    python

--- python/atcoder/abc305_c.py
+++ python/atcoder/abc305_c.py
@@ -1,57 +1,20 @@
 h, w = map(int, input().split())
 arr = []
 for i in range(h):
-    arr.append(list(input()))
+    arr.append(input())
 
-h = h - 1
-w = w - 1
+up, down, left, right = float('inf'), float('-inf'), float('inf'), float('-inf')
 
-# 長方形の上の行番号を求める
-found = False
 for i in range(h):
     for j in range(w):
         if arr[i][j] == '#':
-            up = i
-            found = True
-            break
-    if found:
-        break
+            up = min(up, i)
+            down = max(down, i)
+            left = min(left, j)
+            right = max(right, j)
 
-# 長方形の下の行番号を求める
-found = False
-for i in range(h, 0, -1):
-    for j in range(w):
-        if arr[i][j] == '#':
-            down = i
-            found = True
-            break
-    if found:
-        break
-
-# 長方形の左の列番号を求める
-found = False
-for j in range(w):
-    for i in range(h):
-        if arr[i][j] == '#':
-            left = j
-            found = True
-            break
-    if found:
-        break
-
-# 長方形の右の列番号を求める
-found = False
-for j in range(w, 0, -1):
-    for i in range(h):
-        if arr[i][j] == '#':
-            right = j
-            found = True
-            break
-    if found:
-        break
-
-for i in range(up, down + 1):
-    for j in range(left, right + 1):
+for i in range(up, down+1):
+    for j in range(left, right+1):
         if arr[i][j] == '.':
-            print(i + 1, j + 1)
+            print(i+1, j+1)
             exit()

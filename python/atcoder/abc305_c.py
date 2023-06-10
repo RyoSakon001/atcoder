2c589cee295477eabc395ae49aefb7bd038c9a93
sakon-ziwave <ryo.development0320@gmail.com>
2023-06-10 23:52:07 +0900

    python

--- python/atcoder/abc305_c.py
+++ python/atcoder/abc305_c.py
@@ -6,71 +6,52 @@ for i in range(h):
 h = h - 1
 w = w - 1
 
-
-# 長方形の左上の座標を求める
+# 長方形の上の行番号を求める
 found = False
 for i in range(h):
     for j in range(w):
         if arr[i][j] == '#':
-            p1 = [i, j]
+            up = i
             found = True
             break
     if found:
         break
 
-# 長方形の右上の座標を求める
-found = False
-for i in range(h):
-    for j in range(w, 0, -1):
-        if arr[i][j] == '#':
-            p2 = [i, j]
-            found = True
-            break
-    if found:
-        break
-
-# 長方形の左下の座標を求める
+# 長方形の下の行番号を求める
 found = False
 for i in range(h, 0, -1):
     for j in range(w):
         if arr[i][j] == '#':
-            p3 = [i, j]
+            down = i
             found = True
             break
     if found:
         break
 
-# 長方形の右下の座標を求める
+# 長方形の左の列番号を求める
 found = False
-for i in range(h, 0, -1):
-    for j in range(w, 0, -1):
+for j in range(w):
+    for i in range(h):
         if arr[i][j] == '#':
-            p4 = [i, j]
+            left = j
             found = True
             break
     if found:
         break
 
-def is_edge_lack(p1, p2, p3, p4):
-    return not (p1[0] == p2[0] and p3[0] == p4[0] and p1[1] == p3[1] and p2[1] == p4[1])
+# 長方形の右の列番号を求める
+found = False
+for j in range(w, 0, -1):
+    for i in range(h):
+        if arr[i][j] == '#':
+            right = j
+            found = True
+            break
+    if found:
+        break
 
-def find_lacking_edge(p1, p2, p3, p4):
-    if (not p1[0] == p2[0]) or (not p2[1] == p4[1]):
-        return [p1[0], p4[1]]
-    elif (not p2[1] == p4[1]) or (not p3[0] == p4[0]):
-        return [p3[0], p2[1]]
-    elif (not p3[0] == p4[0]) or (not p1[1] == p3[1]):
-        return [p4[0], p1[1]]
-    elif (not p1[1] == p3[1]) or (not p1[0] == p2[0]):
-        return [p2[0], p3[1]]
-
-if is_edge_lack(p1, p2, p3, p4):
-    p5 = find_lacking_edge(p1, p2, p3, p4)
-    print(p5[0] + 1, p5[1] + 1)
-    exit()
-
-for i in range(p1[0], p3[0]):
-    for j in range(p1[1], p2[1]):
+for i in range(up, down + 1):
+    for j in range(left, right + 1):
         if arr[i][j] == '.':
             print(i + 1, j + 1)
             exit()

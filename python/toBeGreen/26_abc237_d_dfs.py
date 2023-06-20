87618077b10224c41923870935ba78b0f3a6685f
sakon-ziwave <ryo.development0320@gmail.com>
2023-06-20 11:13:38 +0900

    python

---
+++ python/toBeGreen/26_abc237_d_dfs.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+N = int(input())
+S = input()
+
+A = deque([N])
+
+for i in range(N-1, -1, -1):
+    if S[i] == 'R':
+        A.appendleft(i)
+    else:
+        A.append(i)
+
+print(*A)

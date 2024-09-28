0b7f6d67a35e49186ca52e11dfd2a931deee679a
RyoSakon001 <ryo.development0320@gmail.com>
2024-09-28 22:56:22 +0900

    ABC373

--- python/atcoder/abc373_e.py
+++ python/atcoder/abc373_e.py
@@ -1,42 +1,59 @@
-def calculate_votes_needed(N, M, K, A):
-    total_votes_so_far = sum(A)
-    remaining_votes = K - total_votes_so_far
-    S = sorted(A, reverse=True)
-    ans_arr = [0] * N
+def calculate_votes_needed(n,m,k,a):
+    rem = k - sum(a)
 
-    # cnt_greater[V] = number of candidates with votes greater than V
-    cnt_greater = {}
-    for i, v in enumerate(S):
-        if v not in cnt_greater:
-            cnt_greater[v] = i
+    if n == m:
+        print(' '.join(['0'] * n))
+        return
 
-    # For convenience, add an extra element to S to avoid index errors
-    S.append(0)
+    # ord is the list of indices of a, sorted by a[i]
+    ord = sorted(range(n), key=lambda i: a[i])
+    # b is the sorted list of a
+    b = sorted(a)
+    # sumb is the cumulative sum of b
+    sumb = [0] * (n + 1)
+    for i in range(n):
+        sumb[i + 1] = sumb[i] + b[i]
 
-    for i in range(N):
-        Ai = A[i]
-        cnt_greater_i = cnt_greater[Ai]
-        if cnt_greater_i >= M:
-            # Candidate is not in top M
-            BM = S[M-1]  # M-th highest opponent's vote
+    ans = [-1] * n
+
+    def lower_bound(arr, x):
+        left, right = 0, len(arr)
+        while left < right:
+            mid = (left + right) // 2
+            if arr[mid] < x:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+    for i in range(n):
+        l = -1
+        r = rem + 1
+        while r - l > 1:
+            mid = (l + r) // 2
+            x = b[i] + mid + 1
+            rid = lower_bound(b, x)
+            lid = n - m - (1 if i >= n - m else 0)
+            cnt = 0
+            if rid > lid:
+                cnt += (rid - lid) * (b[i] + mid + 1) - (sumb[rid] - sumb[lid])
+            if lid <= i < rid:
+                cnt -= 1
+            else:
+                cnt += mid
+            if cnt > rem:
+                r = mid
+            else:
+                l = mid
+        if l == rem:
+            ans[ord[i]] = -1
         else:
-            # Candidate is in top M
-            BM = S[M]    # (M+1)-th highest vote
-        Ai_plus_R = Ai + remaining_votes
+            ans[ord[i]] = r
 
-        if Ai_plus_R <= BM:
-            ans_arr[i] = -1
-        elif Ai > BM + remaining_votes:
-            ans_arr[i] = 0
-        else:
-            x = (BM + remaining_votes - Ai + 1) // 2
-            ans_arr[i] = x
-
-    return ans_arr
+    return ans
 
 N, M, K = map(int, input().split())
 A = list(map(int, input().split()))
 
 ans = calculate_votes_needed(N, M, K, A)
 print(' '.join(map(str, ans)))
-

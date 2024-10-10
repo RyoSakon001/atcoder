ab890a5f5515374f325c3ceaae185a1430720e43
RyoSakon001 <ryo.development0320@gmail.com>
2024-10-10 21:18:18 +0900

    game

---
+++ javascript/game/key_move.js
@@ -0,0 +1,15 @@
+window.addEventListener("keydown", (e) => {
+  ctx.clearRect(0 ,0, CANVAS_SIZE_W, CANVAS_SIZE_H);
+  slimes.forEach((slime) => {
+    slime.moveByKey(e)
+  })
+})
+
+window.addEventListener("click", (e) => {
+  slimes.forEach((slime) => {
+    const is_same_position = (slime.posX < e.clientX && e.clientX < slime.posX + slime.sizeX) && (slime.posY < e.clientY && e.clientY < slime.posY + slime.sizeY);
+    slime.isActive = is_same_position;
+    slime.img.src =  is_same_position ? "./images/slime_red.png" : "./images/slime.png";
+  })
+})
+
--- javascript/game/slime.js
+++ javascript/game/slime.js
@@ -41,6 +41,14 @@ class Slime {
     }
     this.drawImage()
   }
+
+  moveByMouse(e) {
+    if (this.isActive) {
+      this.posX = e.clientX - this.sizeX / 2
+      this.posY = e.clientY - this.sizeY / 2
+    }
+    this.drawImage()
+  }
 }
 
 function isSamePosition(e, posX, posY, sizeX, sizeY) {
@@ -52,19 +60,3 @@ const slimes = [
   new Slime(240, 240),
   new Slime(360, 240),
 ]
-
-window.addEventListener("keydown", (e) => {
-  ctx.clearRect(0 ,0, CANVAS_SIZE_W, CANVAS_SIZE_H);
-  slimes.forEach((slime) => {
-    slime.moveByKey(e)
-  })
-})
-
-window.addEventListener("click", (e) => {
-  slimes.forEach((slime) => {
-    const is_same_position = (slime.posX < e.clientX && e.clientX < slime.posX + slime.sizeX) && (slime.posY < e.clientY && e.clientY < slime.posY + slime.sizeY);
-    slime.isActive = is_same_position;
-    slime.img.src =  is_same_position ? "./images/slime_red.png" : "./images/slime.png";
-  })
-})
-
